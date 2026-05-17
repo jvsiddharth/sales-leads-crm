9a0cfffb4725e7abf75143670304cf9350f10b82
jvsiddharth <b4ujasiddhu@gmail.com>
2026-05-17 22:49:44 +0530

    deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,7 @@
 import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-
-  build: {
-    chunkSizeWarningLimit: 1000,
-
-    rollupOptions: {
-      output: [
-        {
-          manualChunks(id) {
-            if (id.includes("node_modules/react") || id.includes("node_modules/react-dom")) {
-              return "vendor";
-            }
-
-            if (id.includes("@tanstack/react-query")) {
-              return "query";
-            }
-
-            if (id.includes("framer-motion")) {
-              return "motion";
-            }
-          },
-        },
-      ],
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
